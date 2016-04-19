499f41f599ba02417824d73d8b6c9f7420064273
Mario Gomez <marioseattle@live.com>
2016-04-19 11:22:18 -0700

    First project
    First project

---
+++ Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="FirstStyle.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+           <!--This is a web or xml comment-->
+        <h1>Birthday Calculator</h1>
+        <hr />
+        <p>Choose your birthday</p>
+        <asp:Calendar ID="Calendar1" runat="server">
+        </asp:Calendar>
+        <p>Enter your name<asp:TextBox ID="NameTextBox" runat="server"></asp:TextBox></p>
+        <p>
+        <asp:Button ID="SubmitButtom" runat="server" Text="Submit" OnClick="SubmitButtom_Click" />
+        <asp:Label ID="ResultLabel" runat="server" CssClass="result"></asp:Label>
+        </p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    /* This is the first hands on project we worked on at .Net 172 Sprinf 2016  */
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void SubmitButtom_Click(object sender, EventArgs e)
+    {
+        GetTimeTillBirthday();
+    }
+
+    protected void GetTimeTillBirthday()
+    {
+        DateTime birthDay;
+        if (Calendar1.SelectedDate == null)
+        {
+            birthDay = DateTime.Now;
+        }
+        else
+        {
+            birthDay = Calendar1.SelectedDate;
+
+        }
+
+        string name = NameTextBox.Text;
+
+        TimeSpan daysUntilBirthday = birthDay.Subtract(DateTime.Now);
+        ResultLabel.Text = "Days until Birthday" + Math.Abs(daysUntilBirthday.Days).ToString() + ".And this many hours" + Math.Abs(daysUntilBirthday.Hours).ToString();
+    }
+}
