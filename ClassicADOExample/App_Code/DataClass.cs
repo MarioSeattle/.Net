60d8fb55f35e7535ad2c0e2a76e910af949b1e5e
Mario Gomez <marioseattle@live.com>
2016-04-19 11:26:12 -0700

    ADO Example using bookreviewDB

---
+++ ClassicADOExample/App_Code/DataClass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+///libraries need to talk database
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+/// <summary>
+///This class will connect to the database
+/// It will have mehtods to retrieve the services 
+/// It will also retrieve all the grands for that service
+/// Mario Gomez 4/12/2016
+/// 
+/// 
+/// </summary>
+public class DataClass
+{
+    SqlConnection connect;
+    public DataClass()
+    {
+        connect = new SqlConnection(ConfigurationManager.ConnectionStrings["CommunityAssistConnectionString"].ToString());
+    }//end constructor
+
+    public DataTable GetServices()
+    {
+        DataTable tbl = null;
+
+        string sql = "SELECT authorkey, authorname FROM Author";
+        SqlCommand cmd = new SqlCommand(sql, connect);
+
+        tbl = ReadData(cmd);
+
+
+        return tbl;
+
+
+    }
+
+    public DataTable GetGrants(int AuthorKey)
+    {
+        DataTable tbl = null;
+
+        string sql = "SELECT * FROM book INNER JOIN authorbook ON book.bookkey= authorbook.bookkey WHERE authorkey = @authorkey";
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        cmd.Parameters.AddWithValue("@AuthorKey", AuthorKey);
+
+        SqlDataReader reader = null;
+        tbl = new DataTable();
+
+        connect.Open();
+        reader = cmd.ExecuteReader();
+        tbl.Load(reader);
+        reader.Close();
+        connect.Close();
+
+
+        tbl = ReadData(cmd);
+        return tbl;
+
+    }
+
+    private DataTable ReadData(SqlCommand cmd)
+    {
+        SqlDataReader reader = null;
+       DataTable tbl = new DataTable();
+
+        connect.Open();
+        reader = cmd.ExecuteReader();
+        tbl.Load(reader);
+        reader.Close();
+        connect.Close();
+
+        return tbl;
+    }
+
+}//end of class 
---
+++ ClassicADOExample/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;//Added for database
+
+public partial class _Default : System.Web.UI.Page
+{
+    DataClass dc = new DataClass();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+            LoadDropDownList();
+    }
+    protected void LoadDropDownList()
+    {
+        DataTable tbl = dc.GetServices();
+        DropDownList1.DataSource = tbl;
+        DropDownList1.DataTextField = "AuthorName";
+        DropDownList1.DataValueField = "AuthorKey";
+        DropDownList1.DataBind();
+        DropDownList1.Items.Insert(0,"Choose one");
+
+
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        FilGrid();
+    }
+
+    protected void FilGrid()
+    {
+
+        if (!DropDownList1.SelectedValue.Equals("Choose a Review"))
+
+        {
+            int key = int.Parse(DropDownList1.SelectedValue.ToString());
+            DataTable tbl = dc.GetGrants(key);
+            GridView1.DataSource = tbl;
+            GridView1.DataBind();
+
+        }
+    }
+}
