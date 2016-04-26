e055486069eccf0168911df069ff1ccfb2efeac7
Mario Gomez <marioseattle@live.com>
2016-04-26 11:57:42 -0700

    database interaction with Iservices

---
+++ 4-26-16/App_Code/Service.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+    Community_AssistEntities db = new Community_AssistEntities();
+    public List<string> GetBusinessRules()
+    {
+        var rules = from r in db.BusinessRules
+                    orderby r.BusinessRuleText
+                    select new { r.BusinessRuleText };
+
+        List<string> bRules = new List<string>();
+        foreach(var r in rules)
+        {
+
+            bRules.Add(r.BusinessRuleText.ToString());
+        }
+
+        return bRules;
+    }
+
+    public List<EmployeeInfo> GetEmployeeByPosition(string positionName)
+    {
+        var emps = from e in db.Employees
+                   from p in e.EmployeePositions
+                   where p.Position.PositionName.Equals(positionName)
+                   select new { e.Person.PersonLastName,
+                                e.Person.PersonFirstName,
+                                p.Position.PositionName,
+                                e.EmployeeHireDate
+                   };
+        List<EmployeeInfo> info = new List<EmployeeInfo>();
+
+        foreach (var K in emps)
+        {
+            EmployeeInfo ei = new EmployeeInfo();
+            ei.LastName = K.PersonLastName;
+            ei.FirstName = K.PersonFirstName;
+            ei.JobPosition = K.PositionName;
+            ei.HireDate = K.EmployeeHireDate.ToShortDateString();
+
+            info.Add(ei);
+
+        }
+
+        return info;
+    }
+
+    public List<string> GetPositions()
+    {
+        var poss = from r in db.Positions
+                   orderby r.PositionName
+                   select new { r.PositionName };
+
+        List<string> bPositions = new List<string>();
+        foreach(var r in poss )
+        {
+
+            bPositions.Add(r.PositionName.ToString());
+
+        }
+
+        return bPositions;
+
+    }
+}
