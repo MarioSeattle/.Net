5bdec4b2a981087e9aa53cafd2c9ee9b32df85d3
Mario Gomez <marioseattle@live.com>
2016-05-05 12:03:06 -0700

    Homework Select Web Client

---
+++ 5_3_16/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Mario's C# Exercise 3_5_16</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <asp:DropDownList ID="DropDownVenues" runat="server"></asp:DropDownList>
+
+        <asp:DropDownList ID="DropDownArtist" runat="server"></asp:DropDownList>
+
+
+        <asp:DropDownList ID="DropDownShows" runat="server"></asp:DropDownList>
+
+
+
+        <asp:Button ID="ButtonVenue" runat="server" Text="Get Shows by Venue" OnClick="ButtonVenue_Click" />
+
+        <asp:GridView ID="GridViewVenue_Shows" runat="server"></asp:GridView>
+
+    
+        <asp:Button ID="Button1" runat="server" Text="Get by Shows Artist" OnClick="Button1_Click" />
+
+        <asp:GridView ID="GridViewArtist_Shows" runat="server"></asp:GridView>
+
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ 5_3_16/Default.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    Service_3_5_16.ShowTrackerServiceClient bsc = new Service_3_5_16.ShowTrackerServiceClient();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            FillDropDownVenues();
+            FillDropDownArtist();
+            FillDropDownShows();
+
+        }
+    }
+
+    protected void FillDropDownVenues()
+
+    {
+        string[] Venues = bsc.GetVenues();
+        DropDownVenues.DataSource = Venues;
+        DropDownVenues.DataBind();
+      
+
+    }
+
+    protected void FillDropDownArtist()
+
+    {
+        string[] Artist = bsc.GetArtist();
+        DropDownArtist.DataSource = Artist;
+        DropDownArtist.DataBind();
+
+
+    }
+
+    protected void FillDropDownShows()
+
+    {
+        string[] shows = bsc.GetShows();
+        DropDownShows.DataSource = shows;
+        DropDownShows.DataBind();
+
+
+    }
+
+
+    protected void ButtonVenue_Click(object sender, EventArgs e)
+    {
+        string venue = DropDownVenues.SelectedItem.Text;
+        Service_3_5_16.ShowInfo[] shows = bsc.GetShowsVenue(venue);
+        GridViewVenue_Shows.DataSource = shows;
+        GridViewVenue_Shows.DataBind();
+
+    }
+
+
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string artist = DropDownVenues.SelectedItem.Text;
+        Service_3_5_16.ShowInfo[] shows = bsc.GetShowsVenue(artist);
+        GridViewArtist_Shows.DataSource = shows;
+        GridViewArtist_Shows.DataBind();
+    }
+}
