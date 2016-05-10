8c29b481766f4d030d498fd24cb2f53335e1bc74
Mario Gomez <marioseattle@live.com>
2016-05-10 11:42:28 -0700

    Log In Example 5/10 + Initial files for homewrok

---
+++ LogInRegistryExample/App_Code/LogInService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "LogInService" in code, svc and config file together.
+public class LogInService : ILogInService
+{
+    BookReviewDbEntities db = new BookReviewDbEntities();
+   public bool RegisterReviewer(Reviewer r)
+    {
+        bool result = true;
+
+        int pass = db.usp_NewReviewer(r.ReviewerUserName, r.ReviewerFirstName, r.ReviewerLastName, r.ReviewerEmail, r.ReviewPlainPassword);
+
+        if(pass == -1)
+        {
+            result = false;
+        }
+
+        return result;
+    }
+
+    public int ReviewerLogin(string userName, string password)
+    {
+        int reviewerkey = 0;
+        int result = db.usp_ReviewerLogin(userName, password);
+        if (result != -1)
+        {
+            var key = (from r in db.Reviewers
+                      where r.ReviewerUserName.Equals(userName)
+                      select new { r.ReviewerKey }).FirstOrDefault();
+
+            reviewerkey = key.ReviewerKey;
+        }
+
+        return reviewerkey;
+           
+    }
+}
